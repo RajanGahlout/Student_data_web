312ab71ba43b1f7853cb8fa5af88aee22f60aeb4
RajanGahlout <rajan19me@gmail.com>
2022-08-26 00:45:57 +0530

    My commint

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+ 
+    return render_template('index.html')
+
+@app.route('/Admin')
+def Admin():
+    return render_template('Admin.html')
+
+@app.route('/Student')
+def Student():
+    return render_template('Student.html')    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
